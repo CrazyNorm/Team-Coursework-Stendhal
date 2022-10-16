193bc218f6c0f76a240c0500f51899483fe5ff37
Daniel Gardiner <daniel.gardiner-2@student.manchester.ac.uk>
2022-10-16 12:54:16 +0100

    Tests to make cheating bug visible (remaining 5 floors)

--- tests/games/stendhal/server/maps/quests/ZekielsPracticalTestQuestTest.java
+++ tests/games/stendhal/server/maps/quests/ZekielsPracticalTestQuestTest.java
@@ -80,4 +80,203 @@ public class ZekielsPracticalTestQuestTest extends ZoneAndPlayerTestImpl {
 		assertEquals("int_semos_wizards_tower_1", player.getZone().getName());
 	}
 	
+	@Test
+	public void testFloor2QuestionAnswered() {
+		//Arrange
+		// - sets the quest as if the 2nd question was answered
+		player.setQuest(QUEST_SLOT, "third_step");
+		// - puts the player in the correct zone for the 1st question
+		StendhalRPZone zone = StendhalRPWorld.get().getZone("int_semos_wizards_tower_2");
+		PlayerTestHelper.registerPlayer(player, zone);
+		// - gets the portal to the next question
+		Portal portal = zone.getPortal("portal_to_third_floor");
+		// - moves the player close enough to use the portal
+		player.setPosition(portal.getX(), portal.getY());
+		
+		//Act - try use the portal
+		portal.onUsed(player);
+		
+		//Assert - check if the player has moved to the next question
+		assertEquals("int_semos_wizards_tower_3", player.getZone().getName());
+	}
+	
+	@Test
+	public void testFloor2QuestionSkipped() {
+		//Arrange
+		// - sets the quest as if the 2nd question wasn't answered
+		player.setQuest(QUEST_SLOT, "second_step");
+		// - puts the player in the zone for the 1st question
+		StendhalRPZone zone = StendhalRPWorld.get().getZone("int_semos_wizards_tower_2");
+		PlayerTestHelper.registerPlayer(player, zone);
+		// - gets the portal to the next question
+		Portal portal = zone.getPortal("portal_to_third_floor");
+		// - moves the player close enough to use the portal
+		player.setPosition(portal.getX(), portal.getY());
+		
+		//Act - try use the portal
+		portal.onUsed(player);
+		
+		//Assert - check if the player has moved to the next question
+		assertEquals("int_semos_wizards_tower_2", player.getZone().getName());
+	}
+	
+	@Test
+	public void testFloor3QuestionAnswered() {
+		//Arrange
+		// - sets the quest as if the 3rd question was answered
+		player.setQuest(QUEST_SLOT, "fourth_step");
+		// - puts the player in the correct zone for the 1st question
+		StendhalRPZone zone = StendhalRPWorld.get().getZone("int_semos_wizards_tower_3");
+		PlayerTestHelper.registerPlayer(player, zone);
+		// - gets the portal to the next question
+		Portal portal = zone.getPortal("portal_to_fourth_floor");
+		// - moves the player close enough to use the portal
+		player.setPosition(portal.getX(), portal.getY());
+		
+		//Act - try use the portal
+		portal.onUsed(player);
+		
+		//Assert - check if the player has moved to the next question
+		assertEquals("int_semos_wizards_tower_4", player.getZone().getName());
+	}
+	
+	@Test
+	public void testFloor3QuestionSkipped() {
+		//Arrange
+		// - sets the quest as if the 3rd question wasn't answered
+		player.setQuest(QUEST_SLOT, "third_step");
+		// - puts the player in the zone for the 1st question
+		StendhalRPZone zone = StendhalRPWorld.get().getZone("int_semos_wizards_tower_3");
+		PlayerTestHelper.registerPlayer(player, zone);
+		// - gets the portal to the next question
+		Portal portal = zone.getPortal("portal_to_fourth_floor");
+		// - moves the player close enough to use the portal
+		player.setPosition(portal.getX(), portal.getY());
+		
+		//Act - try use the portal
+		portal.onUsed(player);
+		
+		//Assert - check if the player has moved to the next question
+		assertEquals("int_semos_wizards_tower_3", player.getZone().getName());
+	}
+	
+	@Test
+	public void testFloor4QuestionAnswered() {
+		//Arrange
+		// - sets the quest as if the 4th question was answered
+		player.setQuest(QUEST_SLOT, "fifth_step");
+		// - puts the player in the correct zone for the 1st question
+		StendhalRPZone zone = StendhalRPWorld.get().getZone("int_semos_wizards_tower_4");
+		PlayerTestHelper.registerPlayer(player, zone);
+		// - gets the portal to the next question
+		Portal portal = zone.getPortal("portal_to_fifth_floor");
+		// - moves the player close enough to use the portal
+		player.setPosition(portal.getX(), portal.getY());
+		
+		//Act - try use the portal
+		portal.onUsed(player);
+		
+		//Assert - check if the player has moved to the next question
+		assertEquals("int_semos_wizards_tower_5", player.getZone().getName());
+	}
+	
+	@Test
+	public void testFloor4QuestionSkipped() {
+		//Arrange
+		// - sets the quest as if the 4th question wasn't answered
+		player.setQuest(QUEST_SLOT, "fourth_step");
+		// - puts the player in the zone for the 1st question
+		StendhalRPZone zone = StendhalRPWorld.get().getZone("int_semos_wizards_tower_4");
+		PlayerTestHelper.registerPlayer(player, zone);
+		// - gets the portal to the next question
+		Portal portal = zone.getPortal("portal_to_fifth_floor");
+		// - moves the player close enough to use the portal
+		player.setPosition(portal.getX(), portal.getY());
+		
+		//Act - try use the portal
+		portal.onUsed(player);
+		
+		//Assert - check if the player has moved to the next question
+		assertEquals("int_semos_wizards_tower_4", player.getZone().getName());
+	}
+	
+	@Test
+	public void testFloor5QuestionAnswered() {
+		//Arrange
+		// - sets the quest as if the 5th question was answered
+		player.setQuest(QUEST_SLOT, "sixth_step");
+		// - puts the player in the correct zone for the 1st question
+		StendhalRPZone zone = StendhalRPWorld.get().getZone("int_semos_wizards_tower_5");
+		PlayerTestHelper.registerPlayer(player, zone);
+		// - gets the portal to the next question
+		Portal portal = zone.getPortal("portal_to_sixth_floor");
+		// - moves the player close enough to use the portal
+		player.setPosition(portal.getX(), portal.getY());
+		
+		//Act - try use the portal
+		portal.onUsed(player);
+		
+		//Assert - check if the player has moved to the next question
+		assertEquals("int_semos_wizards_tower_6", player.getZone().getName());
+	}
+	
+	@Test
+	public void testFloor5QuestionSkipped() {
+		//Arrange
+		// - sets the quest as if the 5th question wasn't answered
+		player.setQuest(QUEST_SLOT, "fifth_step");
+		// - puts the player in the zone for the 1st question
+		StendhalRPZone zone = StendhalRPWorld.get().getZone("int_semos_wizards_tower_5");
+		PlayerTestHelper.registerPlayer(player, zone);
+		// - gets the portal to the next question
+		Portal portal = zone.getPortal("portal_to_sixth_floor");
+		// - moves the player close enough to use the portal
+		player.setPosition(portal.getX(), portal.getY());
+		
+		//Act - try use the portal
+		portal.onUsed(player);
+		
+		//Assert - check if the player has moved to the next question
+		assertEquals("int_semos_wizards_tower_5", player.getZone().getName());
+	}
+	
+	@Test
+	public void testFloor6QuestionAnswered() {
+		//Arrange
+		// - sets the quest as if the 6th question was answered
+		player.setQuest(QUEST_SLOT, "last_step");
+		// - puts the player in the correct zone for the 1st question
+		StendhalRPZone zone = StendhalRPWorld.get().getZone("int_semos_wizards_tower_6");
+		PlayerTestHelper.registerPlayer(player, zone);
+		// - gets the portal to the next question
+		Portal portal = zone.getPortal("portal_to_seventh_floor");
+		// - moves the player close enough to use the portal
+		player.setPosition(portal.getX(), portal.getY());
+		
+		//Act - try use the portal
+		portal.onUsed(player);
+		
+		//Assert - check if the player has moved to the next question
+		assertEquals("int_semos_wizards_tower_7", player.getZone().getName());
+	}
+	
+	@Test
+	public void testFloor6QuestionSkipped() {
+		//Arrange
+		// - sets the quest as if the 6th question wasn't answered
+		player.setQuest(QUEST_SLOT, "sixth_step");
+		// - puts the player in the zone for the 1st question
+		StendhalRPZone zone = StendhalRPWorld.get().getZone("int_semos_wizards_tower_6");
+		PlayerTestHelper.registerPlayer(player, zone);
+		// - gets the portal to the next question
+		Portal portal = zone.getPortal("portal_to_seventh_floor");
+		// - moves the player close enough to use the portal
+		player.setPosition(portal.getX(), portal.getY());
+		
+		//Act - try use the portal
+		portal.onUsed(player);
+		
+		//Assert - check if the player has moved to the next question
+		assertEquals("int_semos_wizards_tower_6", player.getZone().getName());
+	}
 }
