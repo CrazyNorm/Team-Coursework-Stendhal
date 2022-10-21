5dd7a589b1c0e6a96c4682d29eee96051dccdb7d
Hank Bell <hank.bell@student.manchester.ac.uk>
2022-10-21 01:30:12 +0100

    Added testing for moving zones with the sheep pet

--- tests/games/stendhal/server/entity/player/PlayerTest.java
+++ tests/games/stendhal/server/entity/player/PlayerTest.java
@@ -21,6 +21,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import java.util.HashMap;
 
@@ -31,7 +32,9 @@ import org.junit.Test;
 
 import games.stendhal.common.KeyedSlotUtil;
 import games.stendhal.common.constants.Nature;
+import games.stendhal.server.core.engine.SingletonRepository;
 import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.core.rp.StendhalRPAction;
 import games.stendhal.server.entity.Entity;
 import games.stendhal.server.entity.Outfit;
 import games.stendhal.server.entity.item.Item;
@@ -41,6 +44,7 @@ import games.stendhal.server.maps.MockStendlRPWorld;
 import marauroa.common.game.RPObject;
 import marauroa.server.game.db.DatabaseFactory;
 import utilities.PlayerTestHelper;
+import games.stendhal.server.entity.creature.Sheep;
 
 public class PlayerTest {
 	private String playername = "player";
@@ -478,16 +482,24 @@ public class PlayerTest {
 		assertThat(player.get("outfit_colors", "dress"), is(nullValue()));
 	}
 	/**
-	 * Test If the message is sent when attempting to change zones while a pet has not caught up
+	 * Test for an error message when zone changing with a pet/sheep
 	 */
 	@Test
-	public void testZoneChangingAllowed() {
-		Player player = PlayerTestHelper.createPlayer("test dummy");
-		
-		//Temporary test
-		assertThat(true, is(true));
+	public void testZoneChanging() {
+		//Assign
+		StendhalRPZone srpz = new StendhalRPZone("0_semos_city",100,100);
+		SingletonRepository.getRPWorld().addRPZone(srpz);
+		Player player2 = PlayerTestHelper.createPlayer("petTest");
+		//Action
+		player2.teleport(srpz, 10, 12, null, null);
+		Sheep testSheep = new Sheep(player2);
+		player2.teleport(srpz, -1, 12, null, null);
+		StendhalRPAction.placeat(srpz, testSheep, 11, 12);
+		player2.handleSimpleCollision(-1, 12);
+		String message = PlayerTestHelper.getPrivateReply(player2);
+		//Assert
+		assertEquals("You must wait for your pet to catch up before moving between zones", message);
 	}
-
 	/**
 	 * Test comparing client version to a known constant
 	 */
