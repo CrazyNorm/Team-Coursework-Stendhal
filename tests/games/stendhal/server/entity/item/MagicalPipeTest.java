f94721632c871f1fbb1bf3a1eaf055b414deb169
j24834et <entian.tu@student.manchester.ac.uk>
2022-11-28 22:18:35 +0000

    Add test for right hand and no pipe
    In previous tests, the pipe is only tested when equipped in left hand.
    In this commit, tests of equipping on right hand is also tested. A test
    of creatures attacking when not holding a pipe is also added.
    
    change file: MagicalPipeTest.java

--- tests/games/stendhal/server/entity/item/MagicalPipeTest.java
+++ tests/games/stendhal/server/entity/item/MagicalPipeTest.java
@@ -27,18 +27,37 @@ public class MagicalPipeTest {
 		assertEquals("You see a Magical Pipe, it can block creatures from attacking you.", pipe.getDescription());
 	}
 	
-	// tests that creatures are charmed when they target a player holding a pipe
+	// tests that creatures are attacking player when not holding a pipe
 	@Test
-	public void testCharmedWhenTargetted() {
+	public void testNotCharmedWhenTargetted() {
 		// create player and equip with pipe
 		final Player charmer = PlayerTestHelper.createPlayer("charmer");
-		PlayerTestHelper.equipWithItemToSlot(charmer, "magical pipe", "lhand");
 		// create a creature and make it target the player
 		final Creature creature = new Creature();
 		charmer.put("id", 1);
 		creature.setTarget(charmer);
 		// check the creature's attack strategy
-		assertTrue("Charmed state not applied to creature", creature.getAttackStrategy() instanceof Charmed);
+		assertFalse("Creature not attacking player", creature.getAttackStrategy() instanceof Charmed);
+	}
+	
+	// tests that creatures are charmed when they target a player holding a pipe
+	@Test
+	public void testCharmedWhenTargetted() {
+		// create player and equip with pipe
+		final Player charmer = PlayerTestHelper.createPlayer("charmer");
+		final Player charmer2 = PlayerTestHelper.createPlayer("charmer2");
+		PlayerTestHelper.equipWithItemToSlot(charmer, "magical pipe", "lhand");
+		PlayerTestHelper.equipWithItemToSlot(charmer2, "magical pipe", "rhand");
+		// create a creature and make it target the player
+		final Creature creature = new Creature();
+		final Creature creature2 = new Creature();
+		charmer.put("id", 1);
+		charmer2.put("id", 1);
+		creature.setTarget(charmer);
+		creature2.setTarget(charmer2);
+		// check the creature's attack strategy
+		assertTrue("Charmed state not applied to creature-left hand", creature.getAttackStrategy() instanceof Charmed);
+		assertTrue("Charmed state not applied to creature-right hand", creature2.getAttackStrategy() instanceof Charmed);
 	}
 	
 	// tests that creatures are charmed when the pipe is equipped
@@ -46,14 +65,19 @@ public class MagicalPipeTest {
 	public void testCharmedWhenEquipped() {
 		// create player and a creature and make it target the player
 		final Player charmer = PlayerTestHelper.createPlayer("charmer");
+		final Player charmer2 = PlayerTestHelper.createPlayer("charmer2");
 		final Creature creature = new Creature();
+		final Creature creature2 = new Creature();
 		charmer.put("id", 1);
+		charmer2.put("id", 1);
 		creature.setTarget(charmer);
-		// equip the player with the pipe
+		creature2.setTarget(charmer2);
+		// equip the players with the pipe
 		PlayerTestHelper.equipWithItemToSlot(charmer, "magical pipe", "lhand");
+		PlayerTestHelper.equipWithItemToSlot(charmer2, "magical pipe", "rhand");
 		// check the creature's attack strategy
-		assertTrue("Charmed state not applied to creature", creature.getAttackStrategy() instanceof Charmed);
-		
+		assertTrue("Charmed state not applied to creature-left hand", creature.getAttackStrategy() instanceof Charmed);
+		assertTrue("Charmed state not applied to creature-right hand", creature2.getAttackStrategy() instanceof Charmed);
 		// unequip the pipe from the player
 		charmer.drop("magical pipe");
 		// check the creature's attack strategy
